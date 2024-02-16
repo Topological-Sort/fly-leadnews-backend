dff968a394ade4d01d405b56c5750cad1d2aa4a4
Topological-Sort <2860520980@qq.com>
2024-02-16 21:57:35 +0800

    仿今日头条项目初始工程

---
+++ heima-leadnews-model/src/main/java/com/heima/model/pojos/User.java
@@ -0,0 +1,53 @@
+package com.heima.model.pojos;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 用户信息表
+ * </p>
+ *
+ * @author 
+ * @since 2024-02-16
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField("username")
+    private String username;
+
+    @TableField("password")
+    private String password;
+
+    @TableField("ai_quota")
+    private Long aiQuota;
+
+    @TableField("video_expires")
+    private LocalDateTime videoExpires;
+
+    @TableField("profession_quota")
+    private Long professionQuota;
+
+    @TableField("create_time")
+    private LocalDateTime createTime;
+
+    @TableField("update_time")
+    private LocalDateTime updateTime;
+
+}
